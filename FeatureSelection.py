49cf9991a25cc83683db34973411aed84f1d0dbb
Ayush Tripathi <101452500+AyushTripathiIITP@users.noreply.github.com>
2023-12-04 01:19:31 +0530

    Update FeatureSelection.py

--- FeatureSelection.py
+++ FeatureSelection.py
@@ -1,13 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov  4 14:13:38 2017
-
-@author: NishitP
-
-Note: before we can train an algorithm to classify fake news labels, we need to extract features from it. It means reducing the mass
-of unstructured data into some uniform set of attributes that an algorithm can understand. For fake news detection, it could be 
-word counts (bag of words). 
-"""
 import DataPrep
 import pandas as pd
 import numpy as np
@@ -21,57 +11,21 @@ from nltk.tokenize import word_tokenize
 from gensim.models.word2vec import Word2Vec
 
 
-#we will start with simple bag of words technique 
-#creating feature vector - document term matrix
+#bag of words technique 
 countV = CountVectorizer()
 train_count = countV.fit_transform(DataPrep.train_news['Statement'].values)
 
-print(countV)
-print(train_count)
-
-#print training doc term matrix
-#we have matrix of size of (10240, 12196) by calling below
-def get_countVectorizer_stats():
-    
-    #vocab size
-    train_count.shape
-
-    #check vocabulary using below command
-    print(countV.vocabulary_)
-
-    #get feature names
-    print(countV.get_feature_names()[:25])
-
-
-#create tf-df frequency features
 #tf-idf 
-tfidfV = TfidfTransformer()
-train_tfidf = tfidfV.fit_transform(train_count)
-
-def get_tfidf_stats():
-    train_tfidf.shape
-    #get train data feature names 
-    print(train_tfidf.A[:10])
-
-
 #bag of words - with n-grams
-#countV_ngram = CountVectorizer(ngram_range=(1,3),stop_words='english')
-#tfidf_ngram  = TfidfTransformer(use_idf=True,smooth_idf=True)
 
 tfidf_ngram = TfidfVectorizer(stop_words='english',ngram_range=(1,4),use_idf=True,smooth_idf=True)
 
-
 #POS Tagging
-tagged_sentences = nltk.corpus.treebank.tagged_sents()
 
-cutoff = int(.75 * len(tagged_sentences))
 training_sentences = DataPrep.train_news['Statement']
- 
-print(training_sentences)
 
 #training POS tagger based on words
 def features(sentence, index):
-    """ sentence: [w1, w2, ...], index: the index of the word """
     return {
         'word': sentence[index],
         'is_first': index == 0,
@@ -98,17 +52,10 @@ def untag(tagged_sentence):
     return [w for w, t in tagged_sentence]
 
 
-
-#Using Word2Vec 
+#Word embedding using pre-loaded word-embedder
 with open("glove.6B.50d.txt", "rb") as lines:
     w2v = {line.split()[0]: np.array(map(float, line.split()[1:]))
            for line in lines}
-
-
-
-#model = gensim.models.Word2Vec(X, size=100) # x be tokenized text
-#w2v = dict(zip(model.wv.index2word, model.wv.syn0))
-
 
 class MeanEmbeddingVectorizer(object):
     def __init__(self, word2vec):
@@ -126,34 +73,3 @@ class MeanEmbeddingVectorizer(object):
                     or [np.zeros(self.dim)], axis=0)
             for words in X
         ])
-
-
-"""
-class TfidfEmbeddingVectorizer(object):
-    def __init__(self, word2vec):
-        self.word2vec = word2vec
-        self.word2weight = None
-        self.dim = len(word2vec.itervalues().next())
-
-    def fit(self, X, y):
-        tfidf = TfidfVectorizer(analyzer=lambda x: x)
-        tfidf.fit(X)
-        # if a word was never seen - it must be at least as infrequent
-        # as any of the known words - so the default idf is the max of 
-        # known idf's
-        max_idf = max(tfidf.idf_)
-        self.word2weight = defaultdict(
-            lambda: max_idf,
-            [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
-
-        return self
-
-    def transform(self, X):
-        return np.array([
-                np.mean([self.word2vec[w] * self.word2weight[w]
-                         for w in words if w in self.word2vec] or
-                        [np.zeros(self.dim)], axis=0)
-                for words in X
-            ])
-
-"""
